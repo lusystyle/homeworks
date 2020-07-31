ebdd297acf65d3c4971beb4649fb512e40fc878a
lusystyle <67215936+lusystyle@users.noreply.github.com>
2020-07-31 14:15:45 +0900

    3weekhomework

--- 3weekhomework.py
+++ 3weekhomework.py
@@ -1,10 +1,9 @@
-
-
-
-
-
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -23,3 +22,9 @@ for tr in trs:
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     print(rank, title, artist)
 
+    doc = {
+        'rank': rank,
+        'title': title,
+        ' artist ': artist  # DB에는 숫자처럼 생긴 문자열 형태로 저장됩니다.
+    }
+    db.music.insert_one(doc)
